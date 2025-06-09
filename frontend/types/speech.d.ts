f335149c84decc97e4211b0e9e958ac8e6742ebb
Dennis McMahon <40324159+dennissolver@users.noreply.github.com>
2025-06-10 00:08:01 +1000

    Update speech.d.ts

--- frontend/types/speech.d.ts
+++ frontend/types/speech.d.ts
@@ -1,6 +1,22 @@
 export {};
 
 declare global {
+  interface SpeechRecognitionEvent extends Event {
+    results: SpeechRecognitionResultList;
+  }
+
+  interface SpeechRecognitionResultList extends Array<SpeechRecognitionResult> {}
+
+  interface SpeechRecognitionResult {
+    isFinal: boolean;
+    0: SpeechRecognitionAlternative;
+  }
+
+  interface SpeechRecognitionAlternative {
+    transcript: string;
+    confidence: number;
+  }
+
   interface Window {
     SpeechRecognition: any;
     webkitSpeechRecognition: any;
