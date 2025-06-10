150a071bee63f1f0b586c42624ede924430bc00d
Dennis McMahon <40324159+dennissolver@users.noreply.github.com>
2025-06-10 16:51:14 +1000

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.JESS_AGENT_URL = "${process.env.NEXT_PUBLIC_JESS_AGENT_URL}";`,
+        }}
+      ></script>
+
       <body style={{ fontFamily: 'Arial, sans-serif', padding: '1rem' }}>
         {children}
       </body>
